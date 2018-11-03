195b02a7b5c4f6daf0caf603e98ac1d92eccc599
kishan <kishand.261194@gmail.com>
2018-11-03 17:22:54 -0400

    completed baboon image segmentation

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -1,10 +1,15 @@
 import numpy as np
 import math
 import sys
-import copy
+import copy, random
 import matplotlib.pyplot as plt
-def euclidian_distance(p1, p2):
-    return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
+import cv2
+
+def euclidian_distance(p1, p2, brg=False):
+    if brg:
+        return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2 + (p2[2]-p1[2])**2)
+    else:
+        return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 
 def get_mean(arr):
     x, y, l =0, 0, len(arr)
@@ -12,6 +17,15 @@ def get_mean(arr):
         x+=i[0]
         y+=i[1]
     return (x/l, y/l)
+
+def get_mean2(arr):
+    x, y, z, l = 0, 0, 0, len(arr)
+    for i in arr:
+        x+=i[0]
+        y+=i[1]
+        z+=i[2]
+    return (x/l, y/l, z/l)
+
 
 def invertDictionary(d):
     myDict = {}
@@ -35,7 +49,7 @@ def plot(cluster_ass, centers, itr, var):
     plt.savefig('task3_'+'iter'+str(itr)+'_'+var+'.jpg')
     plt.clf()
 
-def find_and_plot_kmeans(x, centers):
+def find_and_plot_kmeans(x, centers, k):
     cluster_ass={}
     for i in x:
         cluster_ass[i]=None
@@ -46,12 +60,12 @@ def find_and_plot_kmeans(x, centers):
             temp = copy.deepcopy(cluster_ass)
         cluster=None
         min=sys.maxsize
-        for j in range(0, 3):
+        for j in range(0, k):
             ed=euclidian_distance(x[i], centers[j])
             if min > ed:
                 cluster=j
                 min=ed
-        cluster_ass[x[i]]=cluster
+            cluster_ass[x[i]]=cluster
         i+=1
         if i == len(x):
             count+=1
@@ -60,14 +74,62 @@ def find_and_plot_kmeans(x, centers):
                 break
             else:
                 plot(cluster_ass, centers, count, 'a')
-                for n in range(0,3):
+                for n in range(0,k):
                     centers[n]=get_mean(invertDictionary(cluster_ass)[n])
                 plot(cluster_ass, centers, count, 'b')
+
+
+def find_and_plot_kmeans2(x, centers, k):
+        cluster_ass={}
+        for i in x:
+            for j in i:
+                cluster_ass[tuple(j)]=None
+        i=0
+        count=0
+        while(True):
+            h=0
+            while(h<512):
+                if i==0 and h==0:
+                    temp = copy.deepcopy(cluster_ass)
+                cluster=None
+                min=sys.maxsize
+                for j in range(0, k):
+                    ed=euclidian_distance(x[i][h], centers[j], True)
+                    if min > ed:
+                        cluster=j
+                        min=ed
+                cluster_ass[tuple(x[i][h])]=cluster
+                h+=1
+            i+=1
+            if i == 512 and h== 512:
+                count+=1
+                i=0
+                h=0
+                if temp==cluster_ass:
+                    break
+                else:
+                    for n in range(0,k):
+                        centers[n]=get_mean2(invertDictionary(cluster_ass)[n])
+                    print(centers)
+        return cluster_ass
 
 
 centers=[(6.2, 3.2), (6.6, 3.7), (6.5, 3.0)]
 
 x=[(5.9, 3.2), (4.6, 2.9), (6.2, 2.8), (4.7, 3.2), (5.5, 4.2), (5.0, 3.0),
    (4.9, 3.1), (6.7, 3.1), (5.1, 3.8), (6.0, 3.0)]
+find_and_plot_kmeans(x, centers, 3)
 
-find_and_plot_kmeans(x, centers)
+k_all = [3, 5, 10, 20]
+for k in k_all:
+    org_baboon=cv2.imread('baboon.jpg')
+    centers=[]
+    for i in range(k):
+        centers.append(org_baboon[random.randint(1, 512)][random.randint(1, 512)])
+
+    ca=find_and_plot_kmeans2(org_baboon, centers, k)
+
+    for i in range(512):
+        for j in range(512):
+            org_baboon[i][j]=np.asarray(centers[ca[tuple(org_baboon[i][j])]])
+    cv2.imwrite('task3_baboon_'+str(k)+'.jpg',org_baboon)
